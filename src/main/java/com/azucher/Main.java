a98c064c44cfac9998a03c1d88eb518130ac20d8
alexiszucher <alexiszucher@gmail.com>
2024-11-02 15:44:51 +0100

    Commit pour projet maven

---
+++ src/main/java/com/azucher/Main.java
@@ -0,0 +1,20 @@
+package com.azucher;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    private transient Integer age;
+
+    public static void main(String[] args) {
+        PredicateImpl.exec();
+
+        Locale fr = new Locale("fr");
+        Locale.setDefault(new Locale("en", "US"));
+        var b = ResourceBundle.getBundle("Penguin", fr);
+        System.out.println(b.getString("name"));
+        Set<? extends RuntimeException> mySet = new TreeSet<RuntimeException>();
+    }
+}
