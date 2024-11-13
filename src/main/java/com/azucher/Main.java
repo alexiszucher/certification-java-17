3a0aa6256db13ceff3d71cae7f281d795a31007a
alexiszucher <alexiszucher@gmail.com>
2024-11-13 07:17:08 +0100

    Primitif, reference, text-block

--- src/main/java/com/azucher/Main.java
+++ src/main/java/com/azucher/Main.java
@@ -1,18 +1,13 @@
 package com.azucher;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
-import java.util.Set;
-import java.util.TreeSet;
-
 public class Main {
     private transient Integer age;
 
     public static void main(String[] args) {
-        Locale fr = new Locale("fr");
-        Locale.setDefault(new Locale("en", "US"));
-        var b = ResourceBundle.getBundle("Penguin", fr);
-        System.out.println(b.getString("name"));
-        Set<? extends RuntimeException> mySet = new TreeSet<RuntimeException>();
+        System.out.println("TaTa");
+        String textBlock = """
+                "Java Study Guide"
+                    by Scott & Jeanne""";
+        System.out.println(textBlock);
     }
 }
