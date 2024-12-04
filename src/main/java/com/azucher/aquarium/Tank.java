a392cefec69a3faa8b2ea483b2ce410a9b9bb7eb
alexiszucher <alexiszucher@gmail.com>
2024-12-04 07:29:39 +0100

    Instant, Slash, TranslateEscape et Indent

--- src/main/java/com/azucher/aquarium/Tank.java
+++ src/main/java/com/azucher/aquarium/Tank.java
@@ -1,9 +1,24 @@
 package com.azucher.aquarium;
 
 class Tank {
+    private static final int NUM_SECONDS_PER_MINUTE;
+    private static final int NUM_MINUTES_PER_HOUR;
+    private static final int NUM_SECONDS_PER_HOUR;
+
+    static {
+        NUM_SECONDS_PER_MINUTE = 60;
+        NUM_MINUTES_PER_HOUR = 60;
+    }
+
+    static {
+        NUM_SECONDS_PER_HOUR
+                = NUM_SECONDS_PER_MINUTE * NUM_MINUTES_PER_HOUR;
+    }
+
     public void print(Water water) {
         Water water1 = new Water();
         System.out.println(water);
         int i = 0;
+        i = NUM_MINUTES_PER_HOUR;
     }
 }
---
+++ src/test/java/String/SlashDansStringTest.java
@@ -0,0 +1,27 @@
+package String;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SlashDansStringTest {
+    @Test
+    void le_slash_n_saute_une_ligne_et_est_considere_comme_un_caractere_en_plus_dans_le_length() {
+        String base = "ewe\nsheep";
+        System.out.println(base);
+        Assertions.assertEquals(9, base.length());
+    }
+
+    @Test
+    void le_slash_t_doit_ajouter_un_tab_horizontal_et_est_considere_comme_un_caractere_en_plus_dans_le_length() {
+        String base = "ewe\tsheep\t";
+        System.out.println(base);
+        Assertions.assertEquals(10, base.length());
+    }
+
+    @Test
+    void le_double_slash_met_un_slash_dans_la_string() {
+        String base = "sheep\\t";
+        System.out.println(base);
+        Assertions.assertEquals(7, base.length());
+    }
+}
