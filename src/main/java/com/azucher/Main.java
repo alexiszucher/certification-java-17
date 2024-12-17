417fa6e9c553c866da6d5fbad62902cf194f4d39
alexiszucher <alexiszucher@gmail.com>
2024-12-17 08:17:57 +0100

    Interface Fonctionnelle

--- src/main/java/com/azucher/Main.java
+++ src/main/java/com/azucher/Main.java
@@ -1,8 +1,5 @@
 package com.azucher;
 
-import com.azucher.aquarium.IsSwimable;
-import com.azucher.aquarium.Water;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -10,9 +7,5 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
 
-
-    Water water = new Water();
-        IsSwimable isSwimable = () -> System.out.println("swim");
-        isSwimable.swim();
     }
 }
--- src/main/java/com/azucher/aquarium/Tank.java
+++ src/main/java/com/azucher/aquarium/Tank.java
@@ -1,6 +1,6 @@
 package com.azucher.aquarium;
 
-public sealed class Tank permits Water {
+public class Tank {
     private static final int NUM_SECONDS_PER_MINUTE;
     private static final int NUM_MINUTES_PER_HOUR;
     private static final int NUM_SECONDS_PER_HOUR;
