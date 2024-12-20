6a8deef410d4bada32c45837fa5c4592eaa5da93
alexiszucher <alexiszucher@gmail.com>
2024-12-20 09:35:02 +0100

    Generique

---
+++ src/test/java/Generics/Crate.java
@@ -0,0 +1,17 @@
+package Generics;
+
+public class Crate<T> {
+    private T content;
+
+    public Crate(T content) {
+        this.content = content;
+    }
+
+    public T content() {
+        return content;
+    }
+
+    public <T> T showGeneric(T content) {
+        return content;
+    }
+}
