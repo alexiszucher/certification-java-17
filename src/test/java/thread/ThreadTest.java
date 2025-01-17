f1cb6f2fe9ef1e9e6a04c48af91f957b3769e2d7
alexiszucher <alexiszucher@gmail.com>
2025-01-17 09:35:29 +0100

    Thread

--- src/test/java/thread/ThreadTest.java
+++ src/test/java/thread/ThreadTest.java
@@ -1,10 +1,11 @@
 package thread;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Stream;
 
 public class ThreadTest {
     Runnable printHello = () -> {
@@ -15,6 +16,11 @@ public class ThreadTest {
     };
     Runnable printHola = () -> {
         System.out.println("Hola");
+    };
+    Runnable infiniteLoop = () -> {
+        while (true) {
+
+        }
     };
 
     @Test
@@ -30,7 +36,7 @@ public class ThreadTest {
     }
 
     @Test
-    void doit_obligatoirement_fermer_le_executor_sinon_l_application_ne_s_eteint_jamais() {
+    void doit_obligatoirement_fermer_le_executor_sinon_le_thread_ne_se_ferme_jamais() {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         try {
             executorService.execute(printHello);
@@ -52,5 +58,80 @@ public class ThreadTest {
         } finally {
             executorService.shutdown();
         }
+    }
+
+    @Test
+    void peut_mettre_un_temps_pour_un_thread_avec_le_concurrency_api() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        try {
+            Future<?> future = executorService.submit(printHello);
+            future.get(10, TimeUnit.SECONDS);
+        } catch (TimeoutException | ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    @Test
+    void peut_faire_des_callable_au_lieu_de_runnable() {
+        Callable<String> returnHello = () -> "Hello";
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        try {
+            Future<String> future = executorService.submit(returnHello);
+            String result = future.get(10, TimeUnit.SECONDS);
+            Assertions.assertEquals("Hello", result);
+        } catch (TimeoutException | ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    @Test
+    void peut_faire_des_callable_avec_thread_sans_timeout() {
+        Callable<List<String>> returnList1 = () -> List.of("Hello", "Hi");
+        Callable<List<String>> returnList2 = () -> List.of("Bonjour", "Salut");
+        Callable<List<String>> returnList3 = () -> List.of("Hola", "Hole");
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        try {
+            List<String> result1 = executorService.submit(returnList1).get();
+            List<String> result2 = executorService.submit(returnList2).get();
+            List<String> result3 = executorService.submit(returnList3).get();
+            List<String> results = Stream.of(result1, result2, result3).flatMap(List::stream).toList();
+            Assertions.assertEquals(List.of("Hello", "Hi", "Bonjour", "Salut", "Hola", "Hole"), results);
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            executorService.shutdownNow();
+        }
+    }
+
+    @Test
+    void peut_verifier_que_toutes_les_taches_sont_terminees() throws InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        try {
+            Future<?> future = executorService.submit(printHello);
+            Future<?> future2 = executorService.submit(printBonjour);
+            Future<?> future3 = executorService.submit(printHola);
+            Future<?> future4 = executorService.submit(infiniteLoop);
+        } finally {
+            executorService.shutdown();
+        }
+        executorService.awaitTermination(3, TimeUnit.SECONDS);
+        if (executorService.isTerminated()) {
+            System.out.println("Terminé !");
+        } else {
+            System.out.println("Une tâche était encore en cours mais nous avons forcé son arrêt");
+        }
+    }
+
+    @Test
+    void peut_planifier_une_tache() {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.schedule(printHello, 10, TimeUnit.SECONDS);
+
+        // TODO ATTENTION A CETTE METHODE !!! Si la fonction prend plus de temps que 5 secondes (Le temps mis en période), cela vire au cauchemar !
+        scheduler.scheduleAtFixedRate(printHello, 0, 5, TimeUnit.SECONDS);
     }
 }
