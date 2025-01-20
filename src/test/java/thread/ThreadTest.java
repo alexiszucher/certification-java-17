a3eeb4c6be7fd46474ba4a327fb95bb6bd6342a0
alexiszucher <alexiszucher@gmail.com>
2025-01-20 13:21:37 +0100

    Thread

--- src/test/java/thread/ThreadTest.java
+++ src/test/java/thread/ThreadTest.java
@@ -3,8 +3,14 @@ package thread;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Stream;
 
 public class ThreadTest {
@@ -133,5 +139,131 @@ public class ThreadTest {
 
         // TODO ATTENTION A CETTE METHODE !!! Si la fonction prend plus de temps que 5 secondes (Le temps mis en période), cela vire au cauchemar !
         scheduler.scheduleAtFixedRate(printHello, 0, 5, TimeUnit.SECONDS);
+    }
+
+    // TODO A NE PAS TROP REGARDER
+    @Test
+    void peut_utiliser_volatile_pour_gerer_la_concurence_mais_ne_pas_de_utiliser_de_counter_plus_plus_car_non_atomic_et_thread_unsafe() {
+        Runnable runnable = () -> {
+            // TODO ++counter sur un volatile est thread unsafe
+            System.out.println(++Counter.value);
+        };
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        try {
+            while (Counter.value != 100) {
+                // TODO il est possible que deux valeurs soient les mêmes
+                executorService.submit(runnable);
+            }
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    @Test
+    void peut_utiliser_les_classes_atomiques_et_le_synchronized_pour_gerer_la_concurence_et_etre_thread_safe() {
+        Runnable runnable = () -> {
+            // TODO Cela garantit de passer un seul thread à la fois sur ce bout de code. Les threads voulant également exécuter cette méthode
+            //   sont au statut BLOCKED en attendant que le thread en cours ait fini
+            synchronized (CounterWithAtomic.class) {
+                System.out.println(CounterWithAtomic.value.incrementAndGet());
+            }
+        };
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        try {
+            while (CounterWithAtomic.value.get() != 100) {
+                executorService.submit(runnable);
+            }
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    @Test
+    void peut_mettre_une_methode_avec_le_mot_cle_synchronized() {
+        Runnable runnable = CounterWithAtomic::addCountAndPrintIt;
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        try {
+            while (CounterWithAtomic.value.get() != 100) {
+                executorService.submit(runnable);
+            }
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    @Test
+    void peut_mettre_un_lock_pour_verouiller_un_bout_de_code() {
+        Lock lock = new ReentrantLock();
+        Runnable runnable = () -> {
+            lock.lock();
+            System.out.println(CounterWithAtomic.value.incrementAndGet());
+            lock.unlock();
+        };
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        try {
+            while (CounterWithAtomic.value.get() != 100) {
+                executorService.submit(runnable);
+            }
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    @Test
+    void peut_executer_des_blocks_de_code_en_attendant_que_tous_les_threads_finissent_le_block_en_cours_avant_de_passer_au_suivant() {
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        try {
+            // TODO cela indique que 4 threads doivent passer sur les lions avant de débloquer la barrière
+            CyclicBarrier lionFinished = new CyclicBarrier(4, () -> System.out.println("LION FINISHED !"));
+            executorService.submit(() -> AnimalManager.performTask(lionFinished));
+            executorService.submit(() -> AnimalManager.performTask(lionFinished));
+            executorService.submit(() -> AnimalManager.performTask(lionFinished));
+            executorService.submit(() -> AnimalManager.performTask(lionFinished));
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    @Test
+    void peut_faire_des_traitements_sur_des_listes_avec_les_synchronizedCollection() {
+        List<String> animals = new ArrayList<>();
+        Collection<String> collection = Collections.synchronizedCollection(animals);
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        try {
+            executorService.submit(() -> collection.add("Lion"));
+            executorService.submit(() -> collection.add("Tiger"));
+            executorService.submit(() -> collection.add("Monkey"));
+            collection.forEach(System.out::println);
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    class Counter {
+        // TODO Volatile permet à une variable de n'y avoir accès que par une seule entrée mémoire.
+        //  Mais si nous faisons des fonctions non atomiques (plusieurs choses à la fois comme ++counter), cela devient thread unsafe
+        public volatile static int value = 0;
+    }
+
+    class CounterWithAtomic {
+        public static AtomicInteger value = new AtomicInteger(0);
+
+        public static synchronized void addCountAndPrintIt() {
+            System.out.println(value.incrementAndGet());
+        }
+    }
+
+    class AnimalManager {
+        public static void performTask(CyclicBarrier lionFinished) {
+            try {
+                System.out.println("Add lion");
+                lionFinished.await();
+                System.out.println("Add Tiger");
+            } catch (BrokenBarrierException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
